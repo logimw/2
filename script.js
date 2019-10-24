120786203cc1af9d67220f668cdc5af43bf1212b
Mateusz Wójcik <mateusz-wojcik@hotmail.com>
2019-10-24 16:32:35 +0200

    Added smoothly scrolling

--- script.js
+++ script.js
@@ -10,22 +10,45 @@ let img = document.querySelectorAll(".album__img");
 // let photo = document.querySelector()
 let album = document.querySelector(".album");
 let body = document.querySelector("body");
+const keyEsc = 27;
+
+
+const counter = document.createElement("span");
+counter.classList.add("counter");
+
+
+
+
 
 
 
 let ofLoop;
-for(value of img){
+let i = 0;
+let arr = [];
+for (value of img) {
     value.addEventListener("click", showGallery);
     ofLoop = value;
+    // console.log(value);
+    // console.log(img);
+    // console.log(img[i++]);
+    arr.push(img[i++])
 
 }
+console.log(arr)
+arr.forEach((element, i) => {
+    element.addEventListener("click", ()=>{
+        console.log(i);
+    })
+    console.log(element, i);
+    counter.innerHTML = `${i}`;
+});
 
 
-burger.addEventListener("click", function(){
+burger.addEventListener("click", function () {
     nav.classList.toggle("navigation-active");
 
     navLinks.forEach((link, index) => {
-        if(link.style.animation){
+        if (link.style.animation) {
             link.style.animation = "";
         } else {
             link.style.animation = `navLinkFade 0.5s  ${index / 7 + 0.3}s`
@@ -34,23 +57,53 @@ burger.addEventListener("click", function(){
     burger.classList.toggle("toggle");
 });
 
+function runCounter(){
 
-function showGallery(e){
+}
+
+
+function showGallery(e) {
     let imgAttribute = e.target.getAttribute("src");
+    // const frame = document.createElement("div");
     const photo = document.createElement("img");
-    const showGalleryBlock = document.createElement("div");
 
-    photo.setAttribute("src",`${imgAttribute}`);
-    photo.className = "show-image";
+    const showGalleryBlock = body.appendChild(document.createElement("div"));
+    body.style.position = "relative";
+    
+
+    photo.classList.add("tiny-image");
+    setTimeout(() => {
+        photo.classList.remove("tiny-image");
+        photo.classList.add("show-image");
+    }, 100);
+
+
+    photo.setAttribute("src", `${imgAttribute}`);
+    // photo.className = "show-image";
     showGalleryBlock.className = "show-gallery";
 
     showGalleryBlock.appendChild(photo);
-    gallery.appendChild(showGalleryBlock);
+    showGalleryBlock.appendChild(counter);
+    // gallery.appendChild(showGalleryBlock);
 
-    showGalleryBlock.addEventListener('click', function (){
-        showGalleryBlock.remove();
+
+    showGalleryBlock.addEventListener('click', function () {
+        setTimeout(() => {
+            showGalleryBlock.remove();
+        }, 100);
     })
+    document.body.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        // console.log(e.keyCode);
+        if (e.keyCode === keyEsc) {
+            showGalleryBlock.remove();
+        }
+    });
+
+    
 }
+
+
 
 
 document.addEventListener('scroll', fixBar);
@@ -61,8 +114,7 @@ function fixBar() {
             value.style.color = "#ffffff";
         }
         navBar.classList.replace("top", "low");
-    } 
-    else if(window.innerWidth <= 480) {
+    } else if (window.innerWidth <= 480) {
         navBar.classList.replace("low", "top");
         for (value of navLink) {
             value.style.color = "#fff";
@@ -90,7 +142,10 @@ window.addEventListener("load", function () {
     const galleryHeight = parseInt(getComputedStyle(gallery).height);
     const blogHeight = parseInt(getComputedStyle(blog).height) + parseInt(getComputedStyle(subscribe).height);
     const navBarHeight = parseInt(getComputedStyle(navBar).height);
-    
+
+    const backToTop = document.querySelector(".back-to-top");
+
+
     document.addEventListener('scroll', function () {
         if (window.pageYOffset >= 0 && window.pageYOffset < navBarHeight) {
             navLink[0].classList.remove("active-link");
@@ -99,6 +154,7 @@ window.addEventListener("load", function () {
             navLink[3].classList.remove("active-link");
             navLink[4].classList.remove("active-link");
             navLink[5].classList.remove("active-link");
+            backToTop.classList.remove('button-on');
         } else if (window.pageYOffset > navBarHeight && window.pageYOffset < (headerHeight - navBarHeight)) {
             navLink[0].classList.add("active-link");
             navLink[1].classList.remove("active-link");
@@ -106,6 +162,7 @@ window.addEventListener("load", function () {
             navLink[3].classList.remove("active-link");
             navLink[4].classList.remove("active-link");
             navLink[5].classList.remove("active-link");
+            backToTop.classList.add("button-on")
         } else if (window.pageYOffset > (headerHeight - navBarHeight) && window.pageYOffset <= (aboutHeight + headerHeight - navBarHeight)) {
             navLink[0].classList.remove("active-link");
             navLink[1].classList.add("active-link");
@@ -143,4 +200,10 @@ window.addEventListener("load", function () {
             navLink[5].classList.add("active-link");
         }
     });
+});
+
+
+const scroll = new SmoothScroll('nav a[href*="#"], .back-to-top', {
+    speed: 200,
+    easing: 'easeInCubic'
 });
